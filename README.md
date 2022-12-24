9b98d3dcf210881d6dcf8f2247db478c5de9b99a
julio mota <juliocesarmotapereira@gmail.com>
2022-12-24 20:06:22 -0300

    continuação js

---
+++ README.md
@@ -0,0 +1,7 @@
+## prática de estudos javascript progromando a orientação a objetos
+
+### anotações
+- atalho "ctrl + alt" alterar diversar linhas de uma só vez;
+- molde (classe) {atributos ou campos ou propriedades};
+- operador new;
+- proteção de atributos usamos "_" para indicar que atributo é privado; 
--- js/index.js
+++ js/index.js
@@ -1,2 +1,47 @@
-const cliente1Nome = "Ricardo";
-const cliente1CPF = "11122233300";
+class Cliente {
+    // atributos - class e meu molde
+    nome;
+    cpf;
+    rg;
+   
+}
+
+class ContaCorrente {
+    agencia;
+    // #saldo = 0; https://github.com/tc39/proposal-class-fields#private-fields
+    _saldo = 0;
+
+    // nova estrutura: as funções (ou métodos, nome comum em orientação a objetos)
+    sacar(valor) {
+        if(this._saldo >= valor) {
+            this._saldo -= valor;
+        }
+    }
+
+    // entre (são parâmentros que ele recebe)
+    depositar(valor) {
+        if(valor > 0) {
+            this._saldo += valor;
+        }
+    }
+}
+
+// objeto - instância do meu molde
+const cliente1 = new Cliente(); 
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233300;
+cliente1.rg = "mg-123";
+
+
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 44455566600;
+
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.saldo = 0;
+contaCorrenteRicardo.agencia = 1001;
+
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(50);
+
+console.log(contaCorrenteRicardo);
