f5f6be76767b1fa26d9981891eae7eaf70cf0dcb
Julio Mota <juliocesarmotapereira@gmail.com>
2022-12-27 12:06:14 -0300

    continuação js

--- js/index.js
+++ js/index.js
@@ -14,7 +14,7 @@ cliente2.cpf = 44455566600;
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
 contaCorrenteRicardo.cliente = cliente1;
-contaCorrenteRicardo.depositar(5gi00);
+contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
 conta2.cliente = cliente2;
