3e1a1b2f5e279a9283b3792acb5849c55b3b3b4a
Julio Mota <juliocesarmotapereira@gmail.com>
2022-12-28 23:59:20 -0300

    continuation js

--- js/ContaCorrente.js
+++ js/ContaCorrente.js
@@ -1,31 +1,43 @@
+import { Cliente } from "./Cliente.js";
+
 export class ContaCorrente {
-    agencia;
-    cliente;
+  agencia;
+  _cliente;
 
-    // #saldo = 0; https://github.com/tc39/proposal-class-fields#private-fields
-    _saldo = 0;
-  
-    // nova estrutura: as funções (ou métodos, nome comum em orientação a objetos)
-    sacar(valor) {
-      if (this._saldo >= valor) {
-        this._saldo -= valor;
-        return valor;
-      }
-    }
-  
-    // entre (são parâmentros que ele recebe)
-    depositar(valor) {
-      if (valor <= 0) {
-        return;
-      }
-  
-      this._saldo += valor;
-    }
-
-    tranferir(valor, conta) {
-      
-      const valorSacado = this.sacar(valor);
-      conta.depositar(valorSacado);
-     
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
     }
   }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  // #saldo = 0; https://github.com/tc39/proposal-class-fields#private-fields
+  _saldo = 0;
+
+  // nova estrutura: as funções (ou métodos, nome comum em orientação a objetos)
+  sacar(valor) {
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return valor;
+    }
+  }
+
+  // entre (são parâmentros que ele recebe)
+  depositar(valor) {
+    if (valor <= 0) {
+      return;
+    }
+
+    this._saldo += valor;
+  }
+
+  tranferir(valor, conta) {
+
+    const valorSacado = this.sacar(valor);
+    conta.depositar(valorSacado);
+
+  }
+}
--- js/index.js
+++ js/index.js
@@ -7,9 +7,12 @@ cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233300;
 cliente1.rg = "mg-123";
 
-// const cliente2 = new Cliente();
-// cliente2.nome = "Alice";
-// cliente2.cpf = 44455566600;
+const cliente2 = new Cliente();
+cliente2.nome = "Alice";
+cliente2.cpf = 44455566600;
+
+const cliente3 = new Cliente()
+cliente3.nome = "testeCliente3";
 
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
@@ -17,14 +20,11 @@ contaCorrenteRicardo.cliente = cliente1;
 contaCorrenteRicardo.depositar(500);
 
 const conta2 = new ContaCorrente();
-conta2.cliente = new Cliente;
-conta2.cliente.nome = "Alice";
-conta2.cliente.cpf =  44455566600;
+conta2.cliente = cliente1;
 conta2.agencia = 1002;
 
 let valor = 200;
 contaCorrenteRicardo.tranferir(valor, conta2);
 
-console.log("valor: ", valor)
-console.log(conta2);
-console.log(contaCorrenteRicardo);
+// console.log("valor: ", valor)
+console.log(conta2.cliente);
